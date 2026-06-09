5b57360896dac23114f15160a414e39a1f54f736
Mickael Bouyer <mbouyer@c1r2p4.42nice.fr>
2026-06-09 10:51:39 +0200

    codexion0906

--- log.c
+++ log.c
@@ -6,7 +6,7 @@
 /*   By: mbouyer <mbouyer@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/19 19:34:10 by mickael           #+#    #+#             */
-/*   Updated: 2026/06/03 08:37:07 by mbouyer          ###   ########.fr       */
+/*   Updated: 2026/06/09 10:20:14 by mbouyer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,20 +37,21 @@ void	log_burnout(t_coder *coder, char *msg)
 	t_rules	*r;
 
 	r = coder->rules;
+	pthread_mutex_lock(&r->log_mutex);
+	if (coder->burned_out)
+		return (pthread_mutex_unlock(&r->log_mutex), (void)0);
 	coder->burned_out = 1;
-	pthread_mutex_lock(&coder->rules->log_mutex);
-	ts = elapsed_time_ms(coder->rules->time_start);
+	ts = elapsed_time_ms(r->time_start);
 	printf("%ld %d %s\n", ts, coder->id, msg);
-	pthread_mutex_unlock(&coder->rules->log_mutex);
-	pthread_mutex_lock(&coder->rules->stop_mutex);
-	coder->rules->stopped = 1;
-	pthread_mutex_unlock(&coder->rules->stop_mutex);
-	i = 0;
-	while (i < r->nb_coders)
+	pthread_mutex_unlock(&r->log_mutex);
+	pthread_mutex_lock(&r->stop_mutex);
+	r->stopped = 1;
+	pthread_mutex_unlock(&r->stop_mutex);
+	i = -1;
+	while (++i < r->nb_coders)
 	{
 		pthread_mutex_lock(&r->dongles[i].mutex);
 		pthread_cond_broadcast(&r->dongles[i].cond);
 		pthread_mutex_unlock(&r->dongles[i].mutex);
-		i++;
 	}
 }
